98996cf49e03e2308e1dddefce0b15e28243b8e5
Eric Zimmerman <ericjzim@gmail.com>
2018-09-03 23:53:11 -0500

    initial commit

---
+++ src/tests/simpleTest.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h> // googletest header file
+
+#include <string>
+using std::string;
+
+const char *actualValTrue  = "hello gtest";
+const char *actualValFalse = "hello world";
+const char *expectVal      = "hello gtest";
+
+TEST(StrCompare, CStrEqual) {
+    EXPECT_STREQ(expectVal, actualValTrue);
+}
+
